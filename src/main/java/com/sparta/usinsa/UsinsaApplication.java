4b7c016f541e7d1c28defcef151732a09b004b79
bbobsang <bbobsang@naver.com>
2024-11-25 10:31:46 +0900

    [issue/9] Feat: 프로젝트 초기 설정
    issue #9

---
+++ src/main/java/com/sparta/usinsa/UsinsaApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.usinsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsinsaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UsinsaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sparta/usinsa/UsinsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.usinsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsinsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
