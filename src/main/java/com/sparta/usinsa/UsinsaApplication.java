678c2a3e2bd8f76faa5e01065a2921352957e8ae
jubseok <junseok708@gmail.com>
2024-11-25 20:46:51 +0900

    [#11] fix :EnableCaching 추가,

--- src/main/java/com/sparta/usinsa/UsinsaApplication.java
+++ src/main/java/com/sparta/usinsa/UsinsaApplication.java
@@ -2,8 +2,11 @@ package com.sparta.usinsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableCaching
 public class UsinsaApplication {
 
   public static void main(String[] args) {
