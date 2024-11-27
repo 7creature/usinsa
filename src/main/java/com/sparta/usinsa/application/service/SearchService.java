b644453313041dcdcee3e760c4a9d321340daf1b
jubseok <junseok708@gmail.com>
2024-11-27 23:30:38 +0900

    [#27] feat : 인기 검색어 구축 ,인기 검색어입력 로직 작성

--- src/main/java/com/sparta/usinsa/application/service/SearchService.java
+++ src/main/java/com/sparta/usinsa/application/service/SearchService.java
@@ -1,28 +1,38 @@
 package com.sparta.usinsa.application.service;
 
+import com.sparta.usinsa.domain.entity.Keywords;
 import com.sparta.usinsa.domain.entity.Product;
+import com.sparta.usinsa.domain.repository.KeywordRepository;
 import com.sparta.usinsa.domain.repository.ProductRepository;
 import com.sparta.usinsa.presentation.search.dto.response.SearchResponse;
+import java.time.LocalDateTime;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class SearchService {
 
-  private ProductRepository repository;
+  private ProductRepository productRepository;
+  private KeywordRepository keywordRepository;
+
+  private PopularKeywordService popularKeywordService;
 
   public Page<SearchResponse> searches(int page, int size, String keyword) {
     Pageable pageable = PageRequest.of(page - 1, size);
 
-    Page<Product> products = repository.findAllByNameContaining(pageable, keyword);
+    Page<Product> products = productRepository.findAllByNameContaining(pageable, keyword);
 
     if (products.isEmpty()) {
-      products = repository.findAllByCategory(pageable, keyword);
+      products = productRepository.findAllByCategory(pageable, keyword);
     }
+
+    popularKeywordService.popularKeyword(keyword);
 
     return products
         .map(product -> new SearchResponse(
@@ -31,4 +41,5 @@ public class SearchService {
             product.getName(),
             product.getPrice()));
   }
+
 }
