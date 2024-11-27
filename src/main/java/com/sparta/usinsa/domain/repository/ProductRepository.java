5d5f61fb637dc46606311dc995fbc047e1b70c6b
jubseok <junseok708@gmail.com>
2024-11-27 20:26:21 +0900

    [#24] fix : 검색 기능 생성, ProductRepository 수정

--- src/main/java/com/sparta/usinsa/domain/repository/ProductRepository.java
+++ src/main/java/com/sparta/usinsa/domain/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.sparta.usinsa.domain.repository;
 
 import com.sparta.usinsa.domain.entity.Product;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,9 @@ import org.springframework.stereotype.Repository;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
   List<Product> findByCategory(String category);
+
+  Page<Product> findAllByNameContaining(Pageable pageable,String name);
+
+  Page<Product> findAllByCategory(Pageable pageable, String category);
 }
 
