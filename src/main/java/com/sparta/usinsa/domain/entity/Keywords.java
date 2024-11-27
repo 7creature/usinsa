770d1cd10d2f3744c3befe8e0fe8a239bda231a9
jubseok <junseok708@gmail.com>
2024-11-27 22:20:24 +0900

    [#27] feat : 인기 검색어 구축 ,entity 생성

---
+++ src/main/java/com/sparta/usinsa/domain/entity/Keywords.java
@@ -0,0 +1,33 @@
+package com.sparta.usinsa.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="keywords")
+public class Keywords {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String keyword;
+  private Long searchCount;
+  private LocalDateTime lastSearched;
+
+  public Keywords(String keyword, Long searchCount, LocalDateTime lastSearched) {
+    this.keyword = keyword;
+    this.searchCount = searchCount;
+    this.lastSearched = lastSearched;
+  }
+
+
+}
