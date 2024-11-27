854bab68b7bb61fc80c8a28b693720d08e42ced3
jubseok <junseok708@gmail.com>
2024-11-28 00:51:43 +0900

    [#11] fix : 인기 검색어 캐싱 구축 ,redisConfig 수정

--- src/main/java/com/sparta/usinsa/presentation/common/config/RedisConfig.java
+++ src/main/java/com/sparta/usinsa/presentation/common/config/RedisConfig.java
@@ -1,9 +1,14 @@
 package com.sparta.usinsa.presentation.common.config;
 
+import java.time.Duration;
+import org.apache.catalina.Manager;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -13,7 +18,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching
-public class RedisCacheConfig {
+public class RedisConfig {
   @Value("${spring.data.redis.port}")
   private int port;
 
@@ -33,5 +38,16 @@ public class RedisCacheConfig {
     template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 
     return template;
+  }
+
+  @Bean
+  public CacheManager redisManager(RedisConnectionFactory connectionFactory) {
+    RedisCacheManager cacheManager = RedisCacheManager.builder(connectionFactory)
+        .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofMinutes(60)) // ttl 1시간
+            .disableCachingNullValues())// null값 캐싱하지 않음
+        .transactionAware() // 트렌젝션과 함게 레디스 동작
+        .build();
+
   }
 }
