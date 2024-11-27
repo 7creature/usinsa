343cdb853c99f88d3707bbe9da3792f8c135e031
Dohyeon_Parrk <parkyj3213@naver.com>
2024-11-27 17:35:21 +0900

    feat : (게시글) =상품 조회 카운팅 캐시 ProductControllerV2 컨트롤러

---
+++ src/main/java/com/sparta/usinsa/application/service/ProductServiceV2.java
@@ -0,0 +1,15 @@
+package com.sparta.usinsa.application.service;
+
+import java.util.Set;
+
+public class ProductServiceV2 {
+
+  public void viewProduct(Long productId, String userId) {
+  }
+
+  public Long getProductViewCount(Long productId) {
+  }
+
+  public Set<String> getTopRankedProducts(int limit) {
+  }
+}
