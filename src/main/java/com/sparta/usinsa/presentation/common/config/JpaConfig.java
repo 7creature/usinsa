5aaf35843059ef7422410144fd3a7ec185de2061
yuseonjun <seonjun0906@gmail.com>
2024-11-29 00:46:18 +0900

    [Refactor] 불필요한 Config 삭제

--- src/main/java/com/sparta/usinsa/presentation/common/config/JpaConfig.java
+++ src/main/java/com/sparta/usinsa/presentation/common/config/JpaConfig.java
@@ -1,47 +1,9 @@
 package com.sparta.usinsa.presentation.common.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import java.util.*;
-
-import jakarta.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
 
 @Configuration
-@EnableJpaRepositories(basePackages = "com.sparta.usinsa.domain.repository")
-@EnableTransactionManagement
 @EnableJpaAuditing
 public class JpaConfig {
-
-  @Bean
-  public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-    LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
-    factoryBean.setDataSource(dataSource);
-    factoryBean.setPackagesToScan("com.sparta.usinsa.domain.entity");
-    factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-    factoryBean.setJpaProperties(hibernateProperties());
-    return factoryBean;
-  }
-
-  @Bean
-  public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-    return new JpaTransactionManager(entityManagerFactory);
-  }
-
-  private Properties hibernateProperties() {
-    Properties properties = new Properties();
-    properties.put("hibernate.hbm2ddl.auto", "update");  // DDL 자동 처리 방식
-    properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
-    properties.put("hibernate.show_sql", "true");
-    properties.put("hibernate.format_sql", "true");
-    properties.put("hibernate.use_sql_comments", "true");
-    return properties;
-  }
 }
