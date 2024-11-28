fb8d3f48b23c95d0d25e8c104ae2bc9c5629eda4
jubseok <junseok708@gmail.com>
2024-11-28 11:32:59 +0900

    [#11] fix : 인기 검색어 캐싱 구축 ,캐시적용으로 버전 나눔 및 캐싱 적용

--- src/main/java/com/sparta/usinsa/application/service/SearchService.java
+++ src/main/java/com/sparta/usinsa/application/service/SearchService.java
@@ -6,15 +6,13 @@ import com.sparta.usinsa.domain.repository.KeywordRepository;
 import com.sparta.usinsa.domain.repository.ProductRepository;
 import com.sparta.usinsa.presentation.search.dto.response.KeywordResponse;
 import com.sparta.usinsa.presentation.search.dto.response.SearchResponse;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -44,7 +42,7 @@ public class SearchService {
             product.getPrice()));
   }
 
-  public List<KeywordResponse> popularSearch() {
+  public List<KeywordResponse> V1PopularSearch() {
     List<Keywords> keywords = keywordRepository.findTop10ByOrderBySearchCountDesc();
 
     return keywords
@@ -54,6 +52,20 @@ public class SearchService {
         keyword.getKeyword(),
         keyword.getSearchCount(),
         keyword.getLastSearched()))
+        .toList();
+  }
+
+  @Cacheable(value = "popularSearch", key = "'list'", cacheManager = "caffeineManager")
+  public List<KeywordResponse> V2PopularSearch() {
+    List<Keywords> keywords = keywordRepository.findTop10ByOrderBySearchCountDesc();
+
+    return keywords
+        .stream()
+        .map(keyword -> new KeywordResponse(
+            keyword.getId(),
+            keyword.getKeyword(),
+            keyword.getSearchCount(),
+            keyword.getLastSearched()))
         .toList();
   }
 }
