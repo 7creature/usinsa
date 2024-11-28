ab78c13a44fbac55af0fae3ba669593a58c96443
Dohyeon_Parrk <parkyj3213@naver.com>
2024-11-28 23:25:45 +0900

    fix : commit 충돌 해결

--- src/main/java/com/sparta/usinsa/presentation/common/config/RedisCacheConfig.java
+++ src/main/java/com/sparta/usinsa/presentation/common/config/RedisCacheConfig.java
@@ -1,6 +1,7 @@
 package com.sparta.usinsa.presentation.common.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
