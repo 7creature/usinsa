7687771869dac43097a0946887fb431bd81a330b
jubseok <junseok708@gmail.com>
2024-11-28 23:00:50 +0900

    [#31] fix : 인기 검색어 redis cache적용, 상품 repository import 추가

--- src/main/java/com/sparta/usinsa/domain/repository/ProductRepository.java
+++ src/main/java/com/sparta/usinsa/domain/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.sparta.usinsa.domain.repository;
 
 import com.sparta.usinsa.domain.entity.Product;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
