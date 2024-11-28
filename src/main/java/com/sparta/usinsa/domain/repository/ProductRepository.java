ff2c2d7a1940f54edc126fb78ca430b94685f86c
jubseok <junseok708@gmail.com>
2024-11-28 15:34:42 +0900

    [#11] fix : 인기 검색어 캐싱, 상품 Repository 수정

--- src/main/java/com/sparta/usinsa/domain/repository/ProductRepository.java
+++ src/main/java/com/sparta/usinsa/domain/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.sparta.usinsa.domain.repository;
 
 import com.sparta.usinsa.domain.entity.Product;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +15,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
   Page<Product> findAllByNameContaining(Pageable pageable,String name);
 
   Page<Product> findAllByCategory(Pageable pageable, String category);
+
   List<Product> findByUserId(Long userId);
 }
 
